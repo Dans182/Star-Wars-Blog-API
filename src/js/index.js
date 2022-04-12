9b96efbee711350090e7b86388210fbc52bc9a1b
Dans182 <dgaiteiro@gmail.com>
2022-04-12 15:48:54 +0000

    primer commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//include your index.scss file into the bundle
+//include your index.scss file into the bundle!
 import "../styles/index.css";
 
 //import your own components
