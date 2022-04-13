80e6ee063795e0076216abab684d6eb7cc17ed97
Dans182 <dgaiteiro@gmail.com>
2022-04-13 17:17:50 +0000

    preliminar

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,13 +1,27 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			planets: []
+			planets: [],
+			planetsInfo: [],
+			planetsImg: []
 		},
 		actions: {
 			getPlanets: async () => {
 				const response = await fetch("https://www.swapi.tech/api/planets") 
 				const data = await response.json();
 				setStore({planets: data.results})
+			},
+			getPlanetsInfo: async () => {
+				const response = await fetch("https://www.swapi.tech/api/planets/") 
+				const data = await response.json();
+				setStore({planetsInfo: data.results})
+			},
+
+			getPlanetsImg: async () => {
+				const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/dans182starwars") 
+				const data = await response.json();
+				setStore({planetsImg: data.results})
+
 			}
 	}}
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,38 +1,62 @@
 import React, { useContext, useEffect } from "react";
 import "../../styles/home.css";
 import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+// import rigoImage from "../../img/planetsimg/2.webp";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
   useEffect(() => {
     actions.getPlanets();
+    actions.getPlanetsInfo();
+    actions.getPlanetsImg();
   }, []);
+
+  {
+    store.planets.map((planetImg, j) => {
+      return (
+        <div key={j}>
+          <img src={planetImg.label} className="card-img-top" alt="..." />
+        </div>
+      );
+    });
+  }
 
   return (
     <div className="text-center mt-5">
       <h1>Planets</h1>
-	  <div className="container">
+      <div className="container">
+
         <div className="row scrolling-wrapper-flexbox ">
           {store.planets.map((planet, i) => {
             return (
               <div key={i} className="card">
-                <img src="..." className="card-img-top" alt="..." />
+                <img src="" className="card-img-top" alt="..." />
                 <div className="card-body">
                   <h5 className="card-title">{planet.name}</h5>
-                  <p className="card-text">
+                  
+				  <p className="card-text">
                     Some quick example text to build on the card title and make
                     up the bulk of the card's content.
                   </p>
-                  <button href="#" className="btn btn-grad">
+				  <Link to="/single/:theid"> 
+                  <button className="btn btn-grad">
                     More info
+                  </button>
+				  </Link>
+				  <button href="#" className="btn btn2 btn-grad">
+				  <i className="fa fa-heart" />
                   </button>
                 </div>
               </div>
             );
           })}
+
+		  
         </div>
+
       </div>
-	  </div>
+    </div>
   );
 };
