0557f067d65f97cc01750bbd88a980ad3e248abc
Dans182 <dgaiteiro@gmail.com>
2022-04-13 13:02:31 +0000

    planets + scrollbar

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,36 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext, useEffect } from "react";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  const { store, actions } = useContext(Context);
+
+  useEffect(() => {
+    actions.getPlanets();
+  }, []);
+
+  return (
+    <div className="text-center mt-5">
+      <h1>Planets</h1>
+        <div className="row scrolling-wrapper-flexbox">
+          {store.planets.map((planet, i) => {
+            return (
+              <div key={i} className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{planet.name}</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    More info
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+  );
+};
