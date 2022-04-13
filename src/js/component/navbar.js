ac80dbf07b5eba9b0f6bb8e02dfdc15fb6ea162e
Dans182 <dgaiteiro@gmail.com>
2022-04-13 09:45:00 +0000

    navBar lista

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import starWarsLogo from "../../img/starwars-logo.png";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    /*LOGO*/
+
+    <nav className="navbar navbar-light bg-light mb-2 justify-content-around">
+      <Link to="/">
+        <div className="navbar-brand">
+          <img src={starWarsLogo} style={{ height: "4rem" }}></img>
+        </div>
+      </Link>
+
+      <div className="ml-auto">
+        <Link to="/">
+          <div className="dropdown">
+            <button
+              className="btn btn-primary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Favorites
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li>
+                <a className="dropdown-item" href="#">
+                  (empty)
+                </a>
+              </li>
+            </ul>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
 };
