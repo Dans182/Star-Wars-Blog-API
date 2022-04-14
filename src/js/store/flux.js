9a98fe8bd94acd01895074ad37a316ce0a8554c5
josema9119 <jose.ma.mm.91@gmail.com>
2022-04-14 12:04:20 +0000

    no changes

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,29 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			peoples: [],
+			peopleInfo: [],
+			peopleImg: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			getPlanets: async () => {
+				const response = await fetch("https://www.swapi.tech/api/People") 
+				const data = await response.json();
+				setStore({People: data.results})
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			getPeopleInfo: async () => {
+				const response = await fetch("https://www.swapi.tech/api/People/") 
+				const data = await response.json();
+				setStore({PeopleInfo: data.results})
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+			getPeopleImg: async () => {
+				const response = await fetch("") 
+				const data = await response.json();
+				setStore({PeopleImg: data.results})
 
-				//reset the global store
-				setStore({ demo: demo });
 			}
-		}
-	};
+	}}
 };
 
 export default getState;
