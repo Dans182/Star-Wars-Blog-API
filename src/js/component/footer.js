0de178aa199399751a574f989c462186348b18a1
Dans182 <dgaiteiro@gmail.com>
2022-08-01 21:31:30 +0000

    deploy vercel

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by <i className="fab fa-old-republic display-6"></i>
+			Made with. <i className="fa fa-heart text-danger" /> by <i className="fab fa-old-republic display-6"></i>
 		</p>
 	</footer>
 );
