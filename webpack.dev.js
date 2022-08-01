ea29561263ee25fe543a96e147e38da5f9980172
Dans182 <dgaiteiro@gmail.com>
2022-08-01 21:25:58 +0000

    "vercel config deploy done"

--- webpack.dev.js
+++ webpack.dev.js
@@ -12,18 +12,5 @@ if(process.env.GITPOD_WORKSPACE_URL){
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'cheap-module-source-map',
-    devServer: {
-        port,
-        hot: true,
-        allowedHosts: "all",
-        historyApiFallback: true,
-        static: {
-          directory: path.resolve(__dirname, "dist"),
-        },
-        client: {
-          webSocketURL: publicUrl
-        },
-    },
-    plugins: []
+
 });
