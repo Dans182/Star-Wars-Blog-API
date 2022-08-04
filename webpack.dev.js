876ae0eeb73f089894f2ee0f162153fc6678e3dd
Dans182 <dgaiteiro@gmail.com>
2022-08-04 19:59:44 +0000

    reparacion del webpack para desarrollo + fondo de web app en vercel

--- webpack.dev.js
+++ webpack.dev.js
@@ -12,5 +12,18 @@ if(process.env.GITPOD_WORKSPACE_URL){
 
 module.exports = merge(common, {
     mode: 'development',
-
+    devtool: 'cheap-module-source-map',
+    devServer: {
+        port,
+        hot: true,
+        allowedHosts: "all",
+        historyApiFallback: true,
+        static: {
+          directory: path.resolve(__dirname, "dist"),
+        },
+        client: {
+          webSocketURL: publicUrl
+        },
+    },
+    plugins: []
 });
